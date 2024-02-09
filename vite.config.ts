20d14be85fb2b6b7df5c0b45150e0613ed57358c
theverybusybee <v_antonchik@mail.ru>
2024-02-10 02:28:49 +0300

    ci(config, node_modules): migration to Vite implemented
    React-script files removed, Vite node-modules added, vite-configs added, index.html moved from
    public folder to the root directory and modified in accordance with Vite rules
    
    BREAKING CHANGE: Migration from react-scripts to Vite bundler

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import viteTsconfigPaths from 'vite-tsconfig-paths';
+import eslint from 'vite-plugin-eslint';
+
+export default defineConfig({
+  base: '/',
+  plugins: [react(), viteTsconfigPaths(), eslint()],
+  server: {
+    open: true,
+    port: 3000,
+  },
+});
